78ca81ef1dfa2c4c09dde4df5071b60364aea6ba
pavanshivaG <157051204+pavanshivaG@users.noreply.github.com>
2025-07-22 22:16:39 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,10 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://your-frontend.onrender.com'
+}));
+
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))

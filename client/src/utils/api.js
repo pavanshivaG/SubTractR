87f8db3347110488fa74eb6f89cd5539b6355693
pavanshivaG <157051204+pavanshivaG@users.noreply.github.com>
2025-07-22 21:58:28 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://subtractr-backend.onrender.com' });
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
   if (token) req.headers.Authorization = `Bearer ${token}`;
